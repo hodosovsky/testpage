625e2b91c26f2083d214c849f63368de764ce50d
Mike <hodosovsky@gmail.com>
2022-06-21 17:08:14 +0300

    first_commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+//alert('Putin Huylo')
+
+//let rezult = confirm('putin huylo?');
+//console.log(rezult);
+
+//if (rezult === true) {
+//    alert('Слава Україні');
+/*} 
+else {
+    alert("putin huylo");
+}*/
+
+let text = "I'm from";
+let city = "Cerkassy";
+let Country = 'Ukraine';
+
+console.log(`${text} ${city}, in ${Country}`);
+
+
+
+
+
+
+/* leftBorderWidth = 12;
+console.log(leftBorderWidth);*/
+
