75447a0439f1cf967ca8a362405b0f68e1b74aa9
Mike <hodosovsky@gmail.com>
2022-06-21 20:52:05 +0300

    home work

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 "use strict";
 
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели","");
-console.log(numberOfFilms);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
 
 
+const personalMovieDb = {
+
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const film = prompt("Один из последних просмотреных фильмов:", ""),
+    mark = +prompt("На сколько оцените его:", ""),
+    film2 = prompt("Один из последних просмотреных фильмов:", ""),
+    mark2 = +prompt("На сколько оцените его:", "");
+
+    personalMovieDb.movies[film] = mark;
+    personalMovieDb.movies[film2] = mark2;
+
+console.log(personalMovieDb);
 
 
 
